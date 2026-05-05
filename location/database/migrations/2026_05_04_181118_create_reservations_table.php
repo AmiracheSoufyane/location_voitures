dd06248dbfaf4f4a529a9d6d785963216f72cabf
Soufyan Amirache <soufyanamirache10@gmail.com>
2026-05-05 10:51:45 +0100

    Amelioration table reservation

--- location/database/migrations/2026_05_04_181118_create_reservations_table.php
+++ location/database/migrations/2026_05_04_181118_create_reservations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('user_id')->constrained('user')->cascadeOnDelete();//auth()->id()
+            $table->foreignUuid('user_id')->constrained('users')->cascadeOnDelete();//auth()->id()
             $table->foreignUuid('client_id')->constrained('clients')->cascadeOnDelete();
             $table->foreignUuid('car_id')->constrained('cars')->cascadeOnDelete();
             $table->date('date_start');
